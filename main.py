8c86da23e6482a2eeac7d64059cc57c2ad4ba79c
Dominic Ian Bravo <109084271+Dominic-Bravo@users.noreply.github.com>
2026-05-16 20:37:56 +0800

    fix: added comments for future updates

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from ai_coding_ide.app import AICodingIDE
 
 
 def main() -> None:
+    # root
     root = tk.Tk()
+    # imported widget
     AICodingIDE(root)
+    
     root.mainloop()
+
+    # soon add front ui and configs
 
 
 if __name__ == "__main__":
